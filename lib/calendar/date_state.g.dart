07870edcf4d02a22802b725f20055fedf93612b5
taku-sami <takuya062090@gmail.com>
2020-08-20 20:18:49 +0900

    add freezed file

---
+++ lib/calendar/date_state.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'date_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_DateState _$_$_DateStateFromJson(Map<String, dynamic> json) {
+  return _$_DateState(
+    count: json['count'] as int ?? 0,
+    pageNumber: json['pageNumber'] as int ?? 13,
+  );
+}
+
+Map<String, dynamic> _$_$_DateStateToJson(_$_DateState instance) =>
+    <String, dynamic>{
+      'count': instance.count,
+      'pageNumber': instance.pageNumber,
+    };
